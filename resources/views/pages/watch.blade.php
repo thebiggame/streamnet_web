de84a69a653290ece6a4077f8391fe1ce8356a97
duck <me@duck.me.uk>
2018-11-20 16:59:47 +0000

    Improve styling in some sections
    Update attribution notice
    
    Update target nodejs package versions
    
    Improve stream URL generation on watch page

--- resources/views/pages/watch.blade.php
+++ resources/views/pages/watch.blade.php
@@ -2,12 +2,6 @@
 @section('title', 'Watch tBGLIVE')
 @section('head')
     @parent
-    {{-- <script src="http://vjs.zencdn.net/5.16.0/video.js"></script> --}}
-    {{-- <script src="/resources/js/videojsplugins/videojs.ads.js"></script> --}}
-    {{-- <script src="/resources/js/videojsplugins/videojs-preroll.min.js"></script>--}}
-    {{-- <link href="/vendor/videojs/video-js.min.css" rel="stylesheet" type="text/css"> --}}
-    {{-- <link href="/resources/css/videojsplugins/videojs.ads.css" rel="stylesheet" type="text/css"> --}}
-    {{-- <link href="/resources/css/videojsplugins/videojs-preroll.css" rel="stylesheet" type="text/css"> --}}
     <style>
         body{background-color: rgb(10,10,10);}
         .site-wrapper-content {  margin: 0px; height: 100%;}
@@ -16,7 +10,7 @@
 @endsection
 
 @push('navbar-elements')
-    <span class="navbar-text"><small><kbd>rtmp://stream.biggame/ingest/tbgtv</kbd> <kbd>http://stream.biggame/hls/tbgtv.m3u8</kbd></small></span>
+    <span class="navbar-text"><small><kbd>rtmp://{{request()->getHttpHost()}}/ingest/tbgtv</kbd> <kbd>http://{{request()->getHttpHost()}}/hls/tbgtv.m3u8</kbd></small></span>
     <span class="navbar-text"><span class="fa fa-twitch"></span> <a href="http://www.twitch.tv/tbglan">tbglan</a></span>
     <span class="navbar-text"><a href="javascript:window.open('https://www.twitch.tv/tbglan/chat?popout=','tBG Twitch Chat (kappa)','width=340,height=600')"><span class="fa fa-commenting-o"></span> Chat</a></span>
     <span class="navbar-text"><span class="fa fa-twitter"></span> <a href="http://twitter.com/tbglan">tbglan</a></span>
@@ -26,8 +20,8 @@
     <div class="container-fluid video-container">
                 <div class="videoWrapper">
                     <video id="tbglive" class="video-js vjs-live vjs-default-skin vjs-big-play-centered" controls autoplay width="1000px" data-setup='{"autoplay": true}'>
-                        <source src="http://stream.biggame/dash/tbgtv.mpd" type="application/dash+xml"/>
-                        <source src="http://stream.biggame/hls/tbgtv.m3u8" type="application/x-mpegURL"/>
+                        <source src="{{request()->getSchemeAndHttpHost()}}/dash/tbgtv.mpd" type="application/dash+xml"/>
+                        <source src="{{request()->getSchemeAndHttpHost()}}/hls/tbgtv.m3u8" type="application/x-mpegURL"/>
                     </video>
                 </div>
     </div>
--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="theBIGGAME's streamy paradise..">
-        <meta name="author" content="luaduck for theBIGGAME">
+        <meta name="author" content="duck. @ theBIGGAME">
         <link rel="manifest" href="manifest.json">
         <title>@yield('title') | theBIGGAME</title>
 
@@ -23,15 +23,14 @@
 <body>
 <div class="site-wrapper">
     <nav class="navbar fixed-bottom navbar-expand-md navbar-inverse bg-dark">
-
-        <a class="navbar-brand" href="/"><span class="badge badge-primary badge-pill">tBG{{config('app.eventid', '0')}}</span> <span class="streamnet-font">stream></span></a>
+        <a class="navbar-brand" href="/"><span class="badge badge-primary badge-pill">tBG{{config('app.eventid', '0')}}</span> <span class="streamnet-font">stream</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse mr-auto mt-2 mt-lg-0" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="btn btn-secondary btn-sm interna-font" href="/interna">
+                    <a class="nav-link interna-font" href="/interna">
                         internastream
                     </a>
                 </li>
@@ -44,7 +43,7 @@
     </div>
     @section('footer')
         <div class="footer text-center">
-            &copy; theBIGGAME {{date('Y')}} &centerdot; streamnet by <a href="http://twitter.com/luaduck">@luaduck</a>
+            &copy; theBIGGAME {{date('Y')}} &centerdot; streamnet by <a href="http://twitter.com/duckfullstop">@duckfullstop</a>
         </div>
     @show
 </div>
